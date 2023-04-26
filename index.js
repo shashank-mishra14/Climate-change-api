1e0e6517fb3772926a5eefce2260a94b176cc90b
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-26 18:45:02 +0530

    changes

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the API'})
 })
 
+app.get('/news', (req, res) => {
+    axios.get('https://www.thehindu.com/news/')
+    .then(response => {
+        const html=response.data
+        const $=cheerio.load(html)
+
+        $('a:container("climate")') // looks out for any "a" tag with the word "climate" in it
+    })
+})
+
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
