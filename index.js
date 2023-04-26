ede8cb6d5bc0959db689853fbfa05da899a916b9
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-26 13:57:38 +0530

    changes

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const cheerio=require('cheerio')
 const axios=require('axios')
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to the API'})
+})
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
