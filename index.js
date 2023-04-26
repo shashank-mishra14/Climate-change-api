0cbb989bb044f6ed6fd5c79247c600702b39c00f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-26 21:30:21 +0530

    getting multiple sources

--- index.js
+++ index.js
@@ -7,26 +7,48 @@ const app = express()
 
 const articles=[]
 
+const newspaper=[
+    {
+        name: 'The Hindu',
+        address: 'https://www.thehindu.com/news/'
+    },
+    {
+        name: 'The Times of India',
+        address: 'https://timesofindia.indiatimes.com/'
+    },
+    {
+        name: 'The Indian Express',
+        address: 'https://indianexpress.com/',
+        base: 'https://indianexpress.com/section/climate-change/'
+    }
+]
+
+newspaper.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response=>
+            {
+                const html=response.data
+                const $=cheerio.load(html)
+        
+                $('a:container("climate")',html).each(function () {// looks out for any "a" tag with the word "climate" in it
+                    const title=$(this).text()
+                    const url=$(this).attr('href')
+                    articles.push({
+                        title,
+                        url:newspaper.base + url,
+                        source: newspaper.name
+                    })
+                })
+
+            })
+})
+
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the API'})
 })
 
 app.get('/news', (req, res) => {
-    axios.get('https://www.thehindu.com/news/')
-    .then(response => {
-        const html=response.data
-        const $=cheerio.load(html)
-
-        $('a:container("climate")',html).each(function () {// looks out for any "a" tag with the word "climate" in it
-            const title=$(this).text()
-            const url=$(this).attr('href')
-            articles.push({
-                title,
-                url
-            })
-        }) 
         res.json(articles)
-    }).catch((err) => console.log(err))
 })
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
