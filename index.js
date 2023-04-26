278210277ab9d3490c57ef95fbce67201bc466d3
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-04-26 21:02:48 +0530

    web scrapping

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const axios=require('axios')
 
 const app = express()
 
+const articles=[]
+
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the API'})
 })
@@ -15,8 +17,16 @@ app.get('/news', (req, res) => {
         const html=response.data
         const $=cheerio.load(html)
 
-        $('a:container("climate")') // looks out for any "a" tag with the word "climate" in it
-    })
+        $('a:container("climate")',html).each(function () {// looks out for any "a" tag with the word "climate" in it
+            const title=$(this).text()
+            const url=$(this).attr('href')
+            articles.push({
+                title,
+                url
+            })
+        }) 
+        res.json(articles)
+    }).catch((err) => console.log(err))
 })
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
